d5d55a3cee038222b2662d4e4faff5b972e50d81
Scott Williams <scottcolemanwilliams@gmail.com>
2019-05-23 13:20:26 -0500

    working on q1

--- Sorting.js
+++ Sorting.js
@@ -1,9 +1,9 @@
 //Understanding merge sort
-//given: 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
+//given: 21, 1,| 26, 45,|| 29, 28,| 2, 9,||| 16, 49,| 39, 27,|| 43, 34,| 46, 40
 //
 //What is resulting list that will be sorted after 3 recursive
 //calls to mergesort?
-//
+//21,1  26,45  29,28  2,9  16,49  39,27  43,34  46,40
 //
 //What is the resulting list that will be sorted after 16 recursive
 //calls to mergesort?
@@ -14,4 +14,23 @@
 //
 //Which two lists would be merged on the 7th merge?
 //
+//
+
+//42  56
+//2
+//4 56
+//24
+// 56
+//2456
+
+
+//28376541
+
+
+
+//merge(2837, 6541, 28376541)
+
+//28376541
+
+//leftIndex
 //
