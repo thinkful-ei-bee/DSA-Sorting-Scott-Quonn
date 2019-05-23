c517328c2d5a331890ed51a2b161b7bfd57a117a
Scott Williams <scottcolemanwilliams@gmail.com>
2019-05-23 10:49:50 -0500

    set up q1

---
+++ Sorting.js
@@ -0,0 +1,17 @@
+//Understanding merge sort
+//given: 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
+//
+//What is resulting list that will be sorted after 3 recursive
+//calls to mergesort?
+//
+//
+//What is the resulting list that will be sorted after 16 recursive
+//calls to mergesort?
+//
+//
+//What are the first 2 lists to be merged?
+//
+//
+//Which two lists would be merged on the 7th merge?
+//
+//
